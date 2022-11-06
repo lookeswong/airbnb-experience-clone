9d60f368d9495f88311d87367e8530ba79d2cdb8
LooKes Wong <lookeswong@gmail.com>
2022-11-06 15:05:22 +0000

    Finish the project

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
-      <img className="card--photo" src={`../../images/${props.img}`}/>
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img className="card--photo" src={`../../images/${props.coverImg}`}/>
       <div className="card--stats">
         <img className="card--star" src="../../images/star.png"/>
-        <span>{props.rating}</span>
-        <span className="grey">({props.reviewCount}) - </span>
+        <span>{props.stats.rating}</span>
+        <span className="grey">({props.stats.reviewCount}) - </span>
         <span className="grey">{props.location}</span>
       </div>
       <p className="card--title">{props.title}</p>
